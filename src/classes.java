abf7e647a518b4b43611ee2302d4f67e822ac07f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-06-22 14:38:04 +0300

    test

--- src/classes.java
+++ src/classes.java
@@ -10,7 +10,7 @@ public class classes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nameObj = scanner.nextLine();
-        Asd.nameObj = new Asd();
+        //Asd.nameObj = new Asd();
         //ime.name = "vvv";
 
         //System.out.println(ime.name);
