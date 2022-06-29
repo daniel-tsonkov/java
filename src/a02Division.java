163a4596335be014038efb0dacbe0d5c815717fa
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-06-29 15:16:35 +0300

    Update

---
+++ src/a02Division.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class a02Division {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int myNumber = Integer.parseInt(scanner.nextLine());
+        String dividet = "Not divisible";
+
+        if (myNumber % 2 == 0){
+            dividet = "The number is divisible by 2";
+        }
+        if (myNumber % 3 == 0) {
+            dividet = "The number is divisible by 3";
+        }
+        if (myNumber % 6 == 0) {
+            dividet = "The number is divisible by 6";
+        }
+        if (myNumber % 7 == 0) {
+            dividet = "The number is divisible by 7";
+        }
+        if (myNumber % 10 == 0) {
+            dividet = "The number is divisible by 10";
+        }
+
+        System.out.println(dividet);
+    }
+}
