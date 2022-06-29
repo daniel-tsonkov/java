43c7cc949be29f0a2ab69086763261bffbadab0c
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-06-29 14:52:26 +0300

    Update

--- src/a01Ages.java
+++ src/a01Ages.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class a01Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int ages = Integer.parseInt(scanner.nextLine());
+
+        if (ages <= 2 ) {
+            System.out.println("baby");
+        }else if (ages > 2 && ages <= 13) {
+            System.out.println("child");
+        }else if (ages > 13 && ages <= 19) {
+            System.out.println("teenager");
+        }else if (ages > 19 && ages <= 65) {
+            System.out.println("adult");
+        }else {
+            System.out.println("elder");
+        }
+    }
 }
