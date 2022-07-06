6564e9f037fc38066b864aaeea6cd732ccaa8113
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-06 15:14:49 +0300

    Update

---
+++ src/Arrays/a05PrintPartOfASCIITable.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import  java.util.Scanner;
+
+public class a05PrintPartOfASCIITable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startChar = Integer.parseInt(scanner.nextLine());
+        int endChar = Integer.parseInt(scanner.nextLine());
+
+        for (int i = startChar; i <= endChar; i++) {
+            System.out.print((char) i + " ");
+        }
+    }
+}
