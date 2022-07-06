6c1b91eb9da91efa79912fd361599f4d6d210bd0
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-06 09:46:43 +0300

    Update

--- src/basicSintax/a11RageExpenses.java
+++ src/basicSintax/a11RageExpenses.java
@@ -6,6 +6,22 @@ public class a11RageExpenses {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String myString = scanner.nextLine();
+        int lostGames = Integer.parseInt(scanner.nextLine());
+        float headsetPrice = Float.parseFloat(scanner.nextLine());
+        float mousePrice = Float.parseFloat(scanner.nextLine());
+        float keyboardPrice = Float.parseFloat(scanner.nextLine());
+        float displayPrice = Float.parseFloat(scanner.nextLine());
+
+        int headsets = lostGames / 2;
+        //System.out.println(headsets);
+        int mouses = lostGames / 3;
+        //System.out.println(mouses);
+        int keyboards = lostGames / 6;
+        //System.out.println(keyboards);
+        int displays = keyboards / 2;
+        //System.out.println(displays);
+        float mySum = (headsetPrice * headsets) + (mousePrice * mouses) + (keyboardPrice * keyboards) + (displayPrice * displays);
+
+        System.out.println(String.format("Rage expenses: %.2f lv.", mySum));
     }
 }
