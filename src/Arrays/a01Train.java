89384e10d6e4025bcfe9b7f2047cd77b99673f4d
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-07 19:35:06 +0300

    Update

---
+++ src/Arrays/a01Train.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class a01Train {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberLines = Integer.parseInt(scanner.nextLine());
+        int mySum = 0;
+
+        int[] myArray;
+
+        myArray = new int[numberLines];
+
+        for (int i = 0; i < numberLines; i++) {
+            int oneLine = Integer.parseInt(scanner.nextLine());
+            myArray[i] = oneLine;
+        }
+
+        for (int number : myArray)
+        {
+            mySum += number;
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        System.out.print(mySum);
+    }
+}
