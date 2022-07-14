afb217851b70adf0cf4cc868cedf5543b5601def
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-14 13:56:50 +0300

    Update

---
+++ src/List/a03HouseParty.java
@@ -0,0 +1,13 @@
+package List;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class a03HouseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
