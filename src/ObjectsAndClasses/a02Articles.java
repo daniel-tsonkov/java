ed8b576fdc61d8a7142eb4f36bd48a06f7d34912
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-23 17:46:53 +0300

    Update

--- src/ObjectsAndClasses/a02Articles.java
+++ src/ObjectsAndClasses/a02Articles.java
@@ -7,6 +7,27 @@ public class a02Articles {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] data = scanner.nextLine().split(", ");
+        String title = data[0];
+        String content = data[1];
+        String author = data[2];
+
+        Article article = new Article(title, content, author);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] commands = scanner.nextLine().split(": ");
+
+            switch (commands[0]) {
+                case "Edit":
+                    break;
+                case "ChangeAuthor":
+                    break;
+                case "Rename":
+                    break;
+            }
+        }
     }
 
     static class Article {
@@ -14,10 +35,39 @@ public class a02Articles {
         String content;
         String author;
 
-        public Article (String title, String content, String author) {
+        public Article(String title, String content, String author) {
             this.title = title;
             this.content = content;
             this.author = author;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - %s:%s", this.title, this.content, this.author);
 
         }
     }
