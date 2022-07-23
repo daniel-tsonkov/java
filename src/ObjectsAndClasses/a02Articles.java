de118db4d4a427c93f81a89c08a964259e728abb
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-23 17:24:58 +0300

    Update

--- src/ObjectsAndClasses/a02Articles.java
+++ src/ObjectsAndClasses/a02Articles.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class a02Articles {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+    }
+
+    static class Article {
+        String title;
+        String content;
+        String author;
+
+        public Article (String title, String content, String author) {
+            this.title = title;
+            this.content = content;
+            this.author = author;
+
+        }
     }
 }
