a07e1df6a68bb99c21f820dcbc7355406370a4a6
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-24 10:07:57 +0300

    Update

---
+++ src/ObjectsAndClasses/a03OpinionPool.java
@@ -0,0 +1,11 @@
+package ObjectsAndClasses;
+
+import java.util.Scanner;
+
+public class a03OpinionPool {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+    }
+}
