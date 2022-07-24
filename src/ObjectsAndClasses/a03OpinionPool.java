d87491dd0219c4d177b213ebf060d26a6981f82b
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-07-24 10:40:50 +0300

    Update

--- src/ObjectsAndClasses/a03OpinionPool.java
+++ src/ObjectsAndClasses/a03OpinionPool.java
@@ -1,5 +1,7 @@
 package ObjectsAndClasses;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class a03OpinionPool {
@@ -7,5 +9,28 @@ public class a03OpinionPool {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] data = scanner.nextLine().split(" ");
+            Person person = new Person(data[0], Integer.parseInt(data[1]));
+            people.add(person);
+        }
+        people.stream().sorted((p1, p2) -> p1.name.compareTo(p2.name)).filter(person -> person.age > 30).forEach(p -> System.out.println(p.toString()));
+    }
+
+    static class Person {
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - %d", this.name, this.age);
+        }
     }
 }
