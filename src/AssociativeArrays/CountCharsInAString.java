9b0a483a55dcd94c1ef7a0e6249c3754b9d55891
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-11-21 14:23:10 +0200

    Update

--- src/AssociativeArrays/CountCharsInAString.java
+++ src/AssociativeArrays/CountCharsInAString.java
@@ -1,4 +1,9 @@
 package AssociativeArrays;
 
+import java.util.Scanner;
+
 public class CountCharsInAString {
+    public static void main(String arg[]){
+        Scanner scanner = new Scanner(System.in);
+    }
 }
