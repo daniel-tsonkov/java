13a7f838bea1667223d1697c97a23ed0d22a8550
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-11-23 15:32:48 +0200

    Update

--- src/AssociativeArrays/ForceBook.java
+++ src/AssociativeArrays/ForceBook.java
@@ -1,9 +1,6 @@
 package AssociativeArrays;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class ForceBook {
     public static void main(String[] args) {
@@ -14,12 +11,33 @@ public class ForceBook {
 
         while (!line.equals("Lumpawaroo")) {
             if (line.contains("|")) {
+                String[] data = line.split("\\s+\\|\\s+");
+                String side = data[0];
+                String username = data[1];
+                users.putIfAbsent(side, new ArrayList<>());
+                boolean noneMatch = users.entrySet().stream().noneMatch(entry -> entry.getValue().contains(username));
+                if (noneMatch) {
+                    users.get(side).add(username);
+                }
 
             } else if (line.contains("->")) {
+                String[] data = line.split("\\s+->\\s+");
+                String side = data[1];
+                String username = data[0];
 
+                users.forEach((k, v) -> v.remove(username));
+                users.putIfAbsent(side, new ArrayList<>());
+                users.get(side).add(username);
+                System.out.printf("%s joins the %s side %n", username, side);
             }
 
             line = scanner.nextLine();
         }
+        users.forEach((k, v) -> {
+            if (v.size() != 0) {
+                System.out.printf("Side: %s, Members: %d%n", k, v.size());
+                v.forEach(user -> System.out.printf("! %s%n", user));
+            }
+        });
     }
 }
