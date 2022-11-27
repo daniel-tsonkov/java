931c0a6cf1e41a8b1993aa9cfe80f4940f1e08fc
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-11-27 11:27:34 +0200

    Update

---
+++ src/TextProcessing/ValidUsername.java
@@ -0,0 +1,9 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class ValidUsername {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
