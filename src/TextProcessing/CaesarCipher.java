bfe5003b79515d3ae8d1ab3d79abd7c0af5d8ae7
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-11-29 15:00:45 +0200

    Update

--- src/TextProcessing/CaesarCipher.java
+++ src/TextProcessing/CaesarCipher.java
@@ -6,6 +6,11 @@ public class CaesarCipher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        char[] text = scanner.nextLine().toCharArray();
+
+        for (char c : text) {
+            System.out.print(Character.toString(c + 3));
+        }
 
     }
 }
