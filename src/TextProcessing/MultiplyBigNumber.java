b7c60ee7df774be423cb5b675f99f3bb03164232
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-11-29 15:26:03 +0200

    Update

---
+++ src/TextProcessing/MultiplyBigNumber.java
@@ -0,0 +1,11 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class MultiplyBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
