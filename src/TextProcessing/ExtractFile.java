df63fb46c27a1d99243c245501223cfea1634030
daniel-tsonkov <daniel_tzonkov@abv.bg>
2022-11-29 14:09:22 +0200

    Update

--- src/TextProcessing/ExtractFile.java
+++ src/TextProcessing/ExtractFile.java
@@ -6,6 +6,11 @@ public class ExtractFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String filePath = scanner.nextLine();
+        int lastIndex = filePath.lastIndexOf("\\");
+        int dotIndex = filePath.indexOf('.');
 
+        System.out.printf("File name: %s%n", filePath.substring(lastIndex + 1, dotIndex));
+        System.out.printf("File extension: %s", filePath.substring(dotIndex + 1));
     }
 }
