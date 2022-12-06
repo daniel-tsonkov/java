fc28b6578cf76f9eb177aa9d6bf8165394931fef
Daniel <daniel_tzonkov@abv.bg>
2022-12-06 22:13:15 +0200

    Update

--- src/StaksAndQueue/ReverseNumbersWithAStack.java
+++ src/StaksAndQueue/ReverseNumbersWithAStack.java
@@ -1,11 +1,22 @@
 package StaksAndQueue;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class ReverseNumbersWithAStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] inputNumbers = scanner.nextLine().split("\\s+");
 
+        ArrayDeque <String> stack = new ArrayDeque<>();
+
+        for (String number : inputNumbers) {
+            stack.push(number);
+        }
+
+        for (String element : stack) {
+            System.out.printf("%s ",stack.pop());
+        }
     }
 }
