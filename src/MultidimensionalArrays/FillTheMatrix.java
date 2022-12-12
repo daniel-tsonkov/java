b0e7b20899fc9b453d15aae201ac4216b0e9faac
Daniel <daniel_tzonkov@abv.bg>
2022-12-12 20:10:47 +0200

    Update

---
+++ src/MultidimensionalArrays/FillTheMatrix.java
@@ -0,0 +1,11 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class FillTheMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
