f0f0e2e2c86419e3e945d30918b0113fc1359ec1
Daniel <daniel_tzonkov@abv.bg>
2022-12-12 22:28:02 +0200

    Update

--- src/MultidimensionalArrays/DiagonalDifference.java
+++ src/MultidimensionalArrays/DiagonalDifference.java
@@ -6,28 +6,37 @@ public class DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int matrixSize = Integer.parseInt(scanner.nextLine());
-        int [][] myMatrix = new int[matrixSize][matrixSize];
-        int primaryDiagonal = 0;
-        int secondaryDiagonal = 0;
+        int[][] myMatrix = new int[matrixSize][matrixSize];
 
-        //String[] entryDate = scanner.nextLine().split("\\s+");
+
+
 
         for (int rol = 0; rol < matrixSize; rol++) {
 
             for (int col = 0; col < matrixSize; col++) {
                 myMatrix[rol][col] = scanner.nextInt();
             }
-            //entryDate = scanner.nextLine().split("\\s+");
         }
 
-        for (int i = 0; i < matrixSize; i++) {
-            primaryDiagonal += myMatrix [i][i];
-        }
+        int sumPrimary = getSumOfPrymary(myMatrix);
+        int sumSecondary = getSumOfSecondary(myMatrix);
 
-        for (int i = matrixSize - 1; i >= 0 ; i--) {
-            secondaryDiagonal += myMatrix[matrixSize - 1 - i][i];
-        }
+        System.out.println(Math.abs(sumPrimary - sumSecondary));
+    }
 
-        System.out.println(Math.abs(primaryDiagonal - secondaryDiagonal));
+    private static int getSumOfSecondary(int[][] myMatrix) {
+        int secondaryDiagonal = 0;
+        for (int i = myMatrix.length - 1; i >= 0; i--) {
+            secondaryDiagonal += myMatrix[myMatrix.length - 1 - i][i];
+        }
+        return  secondaryDiagonal;
+    }
+
+    private static int getSumOfPrymary(int[][] myMatrix) {
+        int primaryDiagonal = 0;
+        for (int i = 0; i < myMatrix.length; i++) {
+            primaryDiagonal += myMatrix[i][i];
+        }
+        return primaryDiagonal;
     }
 }
