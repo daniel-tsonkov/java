45796c898836628d431e904e8fddc9cc9f46a0f7
Daniel <daniel_tzonkov@abv.bg>
2022-12-12 22:11:53 +0200

    Update

--- src/MultidimensionalArrays/DiagonalDifference.java
+++ src/MultidimensionalArrays/DiagonalDifference.java
@@ -5,5 +5,29 @@ import java.util.Scanner;
 public class DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int matrixSize = Integer.parseInt(scanner.nextLine());
+        int [][] myMatrix = new int[matrixSize][matrixSize];
+        int primaryDiagonal = 0;
+        int secondaryDiagonal = 0;
+
+        //String[] entryDate = scanner.nextLine().split("\\s+");
+
+        for (int rol = 0; rol < matrixSize; rol++) {
+
+            for (int col = 0; col < matrixSize; col++) {
+                myMatrix[rol][col] = scanner.nextInt();
+            }
+            //entryDate = scanner.nextLine().split("\\s+");
+        }
+
+        for (int i = 0; i < matrixSize; i++) {
+            primaryDiagonal += myMatrix [i][i];
+        }
+
+        for (int i = matrixSize - 1; i >= 0 ; i--) {
+            secondaryDiagonal += myMatrix[matrixSize - 1 - i][i];
+        }
+
+        System.out.println(Math.abs(primaryDiagonal - secondaryDiagonal));
     }
 }
