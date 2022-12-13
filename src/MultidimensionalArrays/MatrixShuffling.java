c3bd42a0369b577de31dc3b0bd40779dbd8bde68
Daniel <daniel_tzonkov@abv.bg>
2022-12-13 20:38:23 +0200

    Update

---
+++ src/MultidimensionalArrays/MatrixShuffling.java
@@ -0,0 +1,9 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MatrixShuffling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
