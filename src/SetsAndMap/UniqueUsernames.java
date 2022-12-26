3a4dd17535b37a219b0ceb1857f7af572f958b40
Daniel <daniel_tzonkov@abv.bg>
2022-12-26 14:28:12 +0200

    Update

---
+++ src/SetsAndMap/UniqueUsernames.java
@@ -0,0 +1,9 @@
+package SetsAndMap;
+
+import java.util.Scanner;
+
+public class UniqueUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
