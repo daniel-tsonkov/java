469c6d785ebb4ad3c7af676e56bfad63f86cc075
Daniel <daniel_tzonkov@abv.bg>
2022-12-28 18:15:47 +0200

    Update

--- src/SetsAndMap/Phonebook.java
+++ src/SetsAndMap/Phonebook.java
@@ -1,9 +1,33 @@
 package SetsAndMap;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Phonebook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> phonebook = new HashMap<>();
+
+        String data = scanner.nextLine();
+
+        while (!data.equals("search")) {
+            String name = data.split("-")[0];
+            String phoneNumer = data.split("-")[1];
+            phonebook.put(name, phoneNumer);
+            data = scanner.nextLine();
+        }
+
+        String name = scanner.nextLine();
+        while (!name.equals("stop")) {
+            if (phonebook.containsKey(name)) {
+                System.out.println(name + " -> " + phonebook.get(name));
+            } else {
+                System.out.println("Contact " + name + " does not exist.");
+            }
+
+            name = scanner.nextLine();
+        }
     }
 }
