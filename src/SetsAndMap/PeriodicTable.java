2e210be4c94f13057e783c8a13f0ebe49938017e
Daniel <daniel_tzonkov@abv.bg>
2022-12-28 17:51:11 +0200

    Update

--- src/SetsAndMap/PeriodicTable.java
+++ src/SetsAndMap/PeriodicTable.java
@@ -1,9 +1,19 @@
 package SetsAndMap;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class PeriodicTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> uniqueElements = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] chemicalElements = scanner.nextLine().split("\\s+");
+
+            uniqueElements.addAll(Arrays.asList(chemicalElements));
+        }
+
+        uniqueElements.forEach(e -> System.out.print(e + " "));
     }
 }
