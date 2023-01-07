80a6d64f5ee855b7db44bfdb6691c4b13ff9d2cd
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-01-07 19:40:14 +0200

    Update

--- src/Swing/MyFrame.java
+++ src/Swing/MyFrame.java
@@ -4,28 +4,52 @@ import com.fazecast.jSerialComm.SerialPort;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 
 public class MyFrame extends JFrame implements ActionListener {
-    MyFrame(){
+    JComboBox runPort;
+
+    MyFrame() {
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
         SerialPort[] s = SerialPort.getCommPorts();
-        for (int i = 0; i < 5; i++) {
-            
-        }
-        String[] ports = {"1", "2", "3"};
-        JComboBox comboBox = new JComboBox(ports);
+        LinkedList<String> ports = new LinkedList<String>();
 
-        this.add(comboBox);
+        for (SerialPort port : s) {
+            ports.add(port.getSystemPortName());
+        }
+        String[] myPorts = new String[ports.size()];
+        for (int i = 0; i < ports.size(); i++) {
+            myPorts[i] = ports.get(i);
+        }
+
+        runPort = new JComboBox(myPorts);
+        runPort.addActionListener(this);
+
+        this.add(runPort);
         this.pack();
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == runPort) {
+            String openPort = runPort.getSelectedItem().toString();
+            SerialPort sp = SerialPort.getCommPort(openPort);//COM9
+            sp.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
+            sp.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0); // block until bytes can be written
 
+            if (sp.openPort()) {
+                System.out.println(openPort + " is open :)");
+            } else {
+                System.out.println("Failed to open port :(");
+                return;
+            }
+        }
     }
 }
