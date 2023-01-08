c4ee3d10989d994fcdca5f023b2dbe460388feba
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-01-08 15:16:07 +0200

    Update

--- src/Swing/TestCOM.java
+++ src/Swing/TestCOM.java
@@ -1,17 +1,18 @@
 package Swing;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TestCOM {
     public static void main(String[] args) {
-        ImageIcon icon = new ImageIcon("/icon.jpg");
-
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(new Color(52, 73, 94));
         frame.setTitle("COM Port Test");
         frame.setSize(800, 600);
+        frame.setResizable(false);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-        frame.setIconImage(icon.getImage());
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(TestCOM.class.getResource("/icon.jpg")));
     }
 }
