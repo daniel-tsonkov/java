375b318b35f06827eee5aa3f7d7c5fb2d1798001
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-02-06 14:28:07 +0200

    Update

--- src/Razni/html.java
+++ src/Razni/html.java
@@ -8,15 +8,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 public class html {
     public static void main(String[] args) throws IOException {
         URL webpage = null;
         URLConnection conn = null;
-        webpage = new URL("https://www.coinmarketcap.com/currencies/bitcoin/");
+        webpage = new URL("http://192.168.5.61/?LED=OFF");
         //webpage = new URL("https://google.com");
         conn = webpage.openConnection();
-        InputStreamReader reader = new InputStreamReader(conn.getInputStream(), "UTF8");
+        InputStreamReader reader = new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8);
         BufferedReader buffer = new BufferedReader(reader);
         String line = "";
 
