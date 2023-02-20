8b20b107f49ae8086b9277abb1b49d732bb8ede2
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-02-20 17:21:50 +0200

    Update

---
+++ src/FunctionalPrograming/ListOfPredicates.java
@@ -0,0 +1,32 @@
+package FunctionalPrograming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+public class ListOfPredicates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Integer> numers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        BiFunction<Integer, List<Integer>, Boolean> isDivisible = ((number, list) -> {
+            for (int numberInList : list) {
+                if (number % numberInList != 0){
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        for (int number = 1; number <= n; number++) {
+            if (isDivisible.apply(number, numers)){
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
