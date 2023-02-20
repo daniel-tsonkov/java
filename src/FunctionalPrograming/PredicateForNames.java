e3456ccad4d73e8f740e2443d59828bacd324bd0
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-02-20 16:39:51 +0200

    Update

---
+++ src/FunctionalPrograming/PredicateForNames.java
@@ -0,0 +1,17 @@
+package FunctionalPrograming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class PredicateForNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] names = scanner.nextLine().split("\\s+");
+
+        Predicate<String> nameLenght = name -> name.length() <= n;
+        Arrays.stream(names).filter(nameLenght).forEach(System.out::println);
+    }
+}
