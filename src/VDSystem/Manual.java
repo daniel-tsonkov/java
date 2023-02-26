8d7c024f904df820e26d5342e98306d0f845bdcb
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-02-26 16:33:11 +0200

    Update

--- src/VDSystem/Manual.java
+++ src/VDSystem/Manual.java
@@ -1,13 +1,22 @@
 package VDSystem;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Manual extends JFrame implements ActionListener, KeyListener, WindowListener {
     JButton ok;
-    JTextArea manual_text;
+    JTextPane myText;
+    SimpleAttributeSet BlueTitleText;
+    SimpleAttributeSet RedDescText;
+    SimpleAttributeSet GreeDocFooter;
     JScrollPane scrollPane;
+
+    JFileChooser fileChooser = new JFileChooser();
 
     MainScreen mainScreen;
 
@@ -22,34 +31,89 @@ public class Manual extends JFrame implements ActionListener, KeyListener, Windo
         this.setLayout(null);
         this.setVisible(true);
 
-        manual_text = new JTextArea();
+        BlueTitleText = new SimpleAttributeSet();
+        RedDescText = new SimpleAttributeSet();
+        GreeDocFooter = new SimpleAttributeSet();
+
+        StyleConstants.setBold(BlueTitleText, true);
+        StyleConstants.setFontSize(BlueTitleText, 20);
+        StyleConstants.setFontFamily(BlueTitleText, "Verdana");
+        StyleConstants.setForeground(BlueTitleText, Color.BLUE);
+
+        StyleConstants.setForeground(RedDescText, Color.RED);
+
+        StyleConstants.setItalic(GreeDocFooter, true);
+        StyleConstants.setFontSize(GreeDocFooter, 20);
+        StyleConstants.setForeground(GreeDocFooter, Color.GREEN);
+        //myText = new JEditorPane();
+        myText = new JTextPane();
+        myText.setEditable(false);
+        myText.setMargin(new Insets(10, 20, 10, 20));
+        myText.setForeground(Color.BLACK);
+        myText.setBackground(new Color(255, 255, 220));
+        myText.setContentType("text/html");
+        String file = "C:/Users/Dani/IdeaProjects/java/tests/src/VDSystem/resources/favicon.png";
+        Icon icon = new ImageIcon(file);
+        myText.setText("<html>" + "<p style = 'color:rgb(255, 0, 0)'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>" +
+                "<p style = 'color:rgb(0, 255, 0)'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>" +
+                "<p><b>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</b></p>" +
+                //"<img src='C:/Users/Dani/IdeaProjects/java/tests/src/VDSystem/resources/favicon.png' width='100' height='100'>" +
+                "'myText.insertIcon(icon)'" +
+                "</html>");
+        myText.insertIcon(icon);
+        SimpleAttributeSet attr = new SimpleAttributeSet();
+        StyleConstants.setBold(attr, true);
+        String text1 = "<p style = 'color:rgb(0, 255, 0)'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>";
+        Document doc = myText.getStyledDocument();
+        try{
+            doc.insertString(doc.getLength(), text1, attr);
+        }catch (BadLocationException e){
+
+        }
+        //myText.getInputContext("<p style = 'color:rgb(255, 0, 0)'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has</p>");
+
+        /*manual_text = new JTextArea();
         manual_text.setLineWrap(true);
         manual_text.setMargin(new Insets(10, 20, 10, 20));
         manual_text.setForeground(Color.BLACK);
         manual_text.setBackground(new Color(255, 255, 220));
+        Font font = manual_text.getFont();*/
+        //manual_text.setFont(manual_text.getFont().deriveFont(Font.BOLD));
         {
-            manual_text.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
+            /*manual_text.setText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Least sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
                     "\n" +
                     "Why do we use it?\n" +
                     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n" +
                     "\n" +
                     "\n" +
                     "Where does it come from?\n" +
-                    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard Mc Clintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
+                    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard Mc Clinton, a Latin professor at Happen-Sydney College in Virginia, looked up one of the more obscure Latin words, consecrated, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtedly source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Minibus Bonjour et Mamoru\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
                     "\n" +
-                    "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n" +
+                    "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Minibus Bonjour et Mamoru\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rack.\n" +
                     "\n" +
+                    //manual_text.setFont(manual_text.getFont().deriveFont(Font.BOLD))
+                    "<html> <h1>This is a Heading</h1> </html>" +
                     "Where can I get some?\n" +
                     "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.\n" +
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
                     "\n" +
                     "Why do we use it?\n" +
                     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n" +
-                    "\n");
+                    "\n");*/
         }
-        manual_text.setEditable(false);
+        //manual_text.setFont(manual_text.getFont().deriveFont(Font.BOLD));
+        /*manual_text.setFont(manual_text.getFont().deriveFont(Font.ITALIC));
+        Map attributes = font.getAttributes();
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        manual_text.setFont(font.deriveFont(attributes));*/
+        //manual_text.setFont(font.deriveFont(Font.BOLD));
+        //manual_text.append("assassins <b>bold text</b>");
 
-        scrollPane = new JScrollPane(manual_text);
+        //manual_text.setEditable(false);
+
+
+        scrollPane = new JScrollPane(myText);
+        //scrollPane = new JScrollPane(manual_text);
         scrollPane.setBounds(5, 10, 575, 570);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
@@ -61,7 +125,7 @@ public class Manual extends JFrame implements ActionListener, KeyListener, Windo
         ok.setBounds(240, 600, 120, 30);
 
         ok.addActionListener(this);
-        manual_text.addKeyListener(this);
+        myText.addKeyListener(this);
         this.addWindowListener(this);
     }
 
@@ -70,6 +134,15 @@ public class Manual extends JFrame implements ActionListener, KeyListener, Windo
         if (e.getSource() == ok) {
             mainScreen.setEnabled(true);
             this.dispose();
+            /*fileChooser.setFileFilter(new FileNameExtensionFilter("jpg", "png"));
+            int returnValue = fileChooser.showOpenDialog(Manual.this);
+            if(returnValue == JFileChooser.APPROVE_OPTION) {
+                //File file = fileChooser.getSelectedFile();
+                //System.out.println(file);
+                String file = "C:/Users/Dani/IdeaProjects/java/tests/src/VDSystem/resources/favicon.png";
+                Icon icon = new ImageIcon(file);//.getAbsolutePath());
+                myText.insertIcon(icon);
+            }*/
         }
     }
 
