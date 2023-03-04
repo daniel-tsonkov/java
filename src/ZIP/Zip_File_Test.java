6d5c99517c7b71c29e6c92289a58c5c15caef486
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-03-04 21:39:37 +0200

    Update

--- src/ZIP/Zip_File_Test.java
+++ src/ZIP/Zip_File_Test.java
@@ -1,7 +1,34 @@
 package ZIP;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class Zip_File_Test {
     public static void main(String[] args) {
-
+        Zip_File_Test.zip_one_file();
+    }
+    public static void zip_one_file() {
+        byte[] buffer = new byte[1024];
+        try{
+            FileOutputStream fos = new FileOutputStream("E:\\java\\testing.zip");
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            ZipEntry ze= new ZipEntry("testing1.txt");
+            zos.putNextEntry(ze);
+            FileInputStream in = new FileInputStream("E:\\java\\testing.txt");
+            int len;
+            while ((len = in.read(buffer)) > 0) {
+                zos.write(buffer, 0, len);
+            }
+            in.close();
+            zos.closeEntry();
+            //remember close it
+            zos.close();
+            System.out.println("Done");
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
