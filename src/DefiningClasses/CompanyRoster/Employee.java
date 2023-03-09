406ba21495ac93a875c5bbbb1ae41c100fbe2a8e
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-03-09 16:14:10 +0200

    Update

--- src/DefiningClasses/CompanyRoster/Employee.java
+++ src/DefiningClasses/CompanyRoster/Employee.java
@@ -7,4 +7,57 @@ public class Employee {
     private String department;
     private String email;
     private int age;
+
+    public Employee(String name, double salary, String position, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+    }
+
+    public Employee(String name, double salary, String position, String department, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(name + " ").append(salary + " ");
+        if (email != null) {
+            builder.append(email + " ");
+        }else {
+            builder.append("N / A ");
+        }
+        if (age != 0) {
+            builder.append(age);
+        }else {
+            builder.append(" -1");
+        }
+        return builder.toString();
+    }
 }
--- src/DefiningClasses/CompanyRoster/Main.java
+++ src/DefiningClasses/CompanyRoster/Main.java
@@ -1,17 +1,57 @@
 package DefiningClasses.CompanyRoster;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         String[] employeeInformation = scanner.nextLine().split("\\s+");
+        Map<String, List<Employee>> departments = new HashMap<>();
         for (int i = 0; i < n; i++) {
             String name = employeeInformation[0];
             double salary = Double.parseDouble(employeeInformation[1]);
             String position = employeeInformation[2];
             String department = employeeInformation[3];
+            Employee employee = null;
+            if (employeeInformation.length == 6) {
+                String email = employeeInformation[4];
+                int age = Integer.parseInt(employeeInformation[5]);
+                new Employee(name, salary, position, department, email, age);
+            } else if (employeeInformation.length == 4) {
+                new Employee(name, salary, position, department);
+            } else if (employeeInformation.length == 5) {
+                if (employeeInformation[4].contains("@")) {
+                    String email = employeeInformation[4];
+                    new Employee(name, salary, position, department, email);
+                } else {
+                    int age = Integer.parseInt(employeeInformation[4]);
+                    new Employee(name, salary, position, department, age);
+                }
+            }
+            if (departments.containsKey(department)) {
+                departments.put(department, new ArrayList<>());
+                departments.get(department).add(employee);
+            } else {
+                departments.get(department).add(employee);
+            }
         }
+        String maxAverageSalaryDepartment = departments.entrySet()
+                .stream()
+                .max(Comparator.comparingDouble(entry -> getAverageSalary(entry.getValue())))
+                .get()
+                .getKey();
+        System.out.printf("Highest Average Salary: %s%n", maxAverageSalaryDepartment);
+        List<Employee> employeeList = departments.get(maxAverageSalaryDepartment);
+        employeeList.sort(Comparator.comparingDouble(employe -> employe.getSalary()));
+        Collections.reverse(employeeList);
+    }
+
+    public static double getAverageSalary(List<Employee> employees) {
+        double sum = 0;
+        for (Employee employee : employees) {
+            sum = sum + employee.getSalary();
+        }
+        return  sum / employees.size();
     }
 }
