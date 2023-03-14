769f39f9bd6796bc3f192889b1f6c591d0bc6523
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-03-14 19:53:06 +0200

    Update

--- src/DefiningClasses/Google/Main.java
+++ src/DefiningClasses/Google/Main.java
@@ -23,30 +23,40 @@ public class Main {
                     String department = parameters[3];
                     double salary = Double.parseDouble(parameters[4]);
                     Company company = new Company(companyName, department, salary);
+                    peopleData.get(personName).setCompany(company);
                     break;
                 case "pocemon":
                     String pokemonName = parameters[2];
                     String pokemonType = parameters[3];
                     Pokemon pokemon = new Pokemon(pokemonName, pokemonType);
+                    peopleData.get(personName).getPokemon().add(pokemon);
                     break;
                 case "parents":
                     String nameParent = parameters[2];
                     String parentBirthday = parameters[3];
                     Parent parent = new Parent(nameParent, parentBirthday);
+                    peopleData.get(personName).getParents().add(parent);
                     break;
                 case "children":
                     String childName = parameters[2];
                     String childBirthday = parameters[3];
                     Child child = new Child(childName, childBirthday);
+                    peopleData.get(personName).getChildren().add(child);
                     break;
                 case "car":
                     String carModel = parameters[2];
                     int carSpeed = Integer.parseInt(parameters[3]);
                     Car car = new Car(carModel, carSpeed);
+                    peopleData.get(personName).setCar(car);
                     break;
             }
 
             information = scanner.nextLine();
         }
+        String searchedPerson = scanner.nextLine();
+        System.out.println(searchedPerson);
+
+        Person personData = peopleData.get(searchedPerson);
+        System.out.println(personData);
     }
 }
--- src/DefiningClasses/Google/Person.java
+++ src/DefiningClasses/Google/Person.java
@@ -23,4 +23,50 @@ public class Person {
         this.children = children;
         this.pokemons = pokemons;
     }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public List<Pokemon> getPokemon() {
+        return this.pokemons;
+    }
+
+    public List<Parent> getParents() {
+        return this.parents;
+    }
+
+    public List<Child> getChildren() {
+        return this.children;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Company:").append("\n");
+        if(company != null) {
+            builder.append(company).append("\n");
+        }
+        builder.append("Car:").append("\n");
+        if(car != null) {
+            builder.append(car).append("\n");
+        }
+        builder.append("Pokemon:").append("\n");
+        for(Pokemon pokemon : pokemons) {
+            builder.append(pokemon).append("\n");
+        }
+        builder.append("Parents:").append("\n");
+        for (Parent parent : parents) {
+            builder.append(parent).append("\n");
+        }
+        builder.append("Children");
+        for(Child child : children) {
+            builder.append(child).append("\n");
+        }
+        return builder.toString();
+    }
 }
