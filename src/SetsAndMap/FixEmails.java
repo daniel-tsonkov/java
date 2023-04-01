d2ca382c034a35464b0a7984b23aaecd116a68aa
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-04-01 10:49:46 +0300

    Update

--- src/SetsAndMap/FixEmails.java
+++
@@ -1,9 +0,0 @@
-package SetsAndMap;
-
-import java.util.Scanner;
-
-public class FixEmails {
-    public static void main(String[] args) {
-        Scanner scaner = new Scanner(System.in);
-    }
-}
