9df21898ce5cc02f3790ea1bb975aad17b444d3e
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-04-08 13:16:45 +0300

    Update

--- src/VerticalFarming/MainScreen.java
+++ src/VerticalFarming/MainScreen.java
@@ -36,7 +36,7 @@ public class MainScreen extends JFrame implements ActionListener {
     public static JButton[] array2dtop = new JButton[154];
     ImageIcon iconExit = new ImageIcon(Toolkit.getDefaultToolkit().getImage(MainScreen.class.getResource("/VerticalFarming/resources/exit.png")));
     JButton exitButton = new JButton(iconExit);
-    int xCell;
+    int cellNumber;
     static String openPort;
     static SerialPort sp;
     OutputStream outputStream1;
@@ -202,28 +202,20 @@ public class MainScreen extends JFrame implements ActionListener {
         tankPanel.setBackground(new Color(0, 0, 0));
         tankPanel.add(tank);
 
-        /*for (xCell = 0; xCell < array2dtop.length; xCell++) {
-            for (int yCell = 0; yCell < array2dtop.length; yCell++) {
-                array2dtop[xCell][yCell] = new JButton("C" + ((xCell * 10) + yCell));
-                array2dtop[xCell][yCell].setActionCommand(String.valueOf((xCell * 10) + yCell));
-                array2dtop[xCell][yCell].setMargin(new Insets(0, 0, 0, 0));
-                array2dtop[xCell][yCell].addActionListener(this);
-                array2dtop[xCell][yCell].setPreferredSize(new Dimension(46, 46));
-                array2dtop[xCell][yCell].setBorder(BorderFactory.createEtchedBorder(0));
-
-                cellPanel.add(array2dtop[xCell][yCell]);
+        for (cellNumber = 0; cellNumber < array2dtop.length; cellNumber++) {
+            array2dtop[cellNumber] = new JButton("C" + (cellNumber + 1));
+            array2dtop[cellNumber].setActionCommand(String.valueOf(cellNumber));
+            array2dtop[cellNumber].setMargin(new Insets(0, 0, 0, 0));
+            array2dtop[cellNumber].setPreferredSize(new Dimension(46, 46));
+            array2dtop[cellNumber].setBorder(BorderFactory.createEtchedBorder(0));
+            if(cellNumber > 140 + 1){
+                array2dtop[cellNumber].setBackground(new Color(130,130,130));
+            } else {
+                array2dtop[cellNumber].setBackground(UIManager.getColor("Button.background"));
             }
-        }*/
-        for (xCell = 0; xCell < array2dtop.length; xCell++) {
-            array2dtop[xCell] = new JButton("C" + (xCell + 1));
-            array2dtop[xCell].setActionCommand(String.valueOf(xCell));
-            array2dtop[xCell].setMargin(new Insets(0, 0, 0, 0));
-            array2dtop[xCell].setPreferredSize(new Dimension(46, 46));
-            array2dtop[xCell].setBorder(BorderFactory.createEtchedBorder(0));
-            array2dtop[xCell].setBackground(UIManager.getColor("Button.background"));
-            array2dtop[xCell].addActionListener(this);
+            array2dtop[cellNumber].addActionListener(this);
 
-            cellPanel.add(array2dtop[xCell]);
+            cellPanel.add(array2dtop[cellNumber]);
         }
     }
 
@@ -393,7 +385,7 @@ public class MainScreen extends JFrame implements ActionListener {
         settingsButton.setPreferredSize(new Dimension(30, 30));
         settingsButton.setBorder(BorderFactory.createEtchedBorder(0));
         toolBar.add(settingsButton);
-        exitButton.setPreferredSize(new Dimension(40, 25));
+        exitButton.setPreferredSize(new Dimension(30, 30));
         toolBar.add(exitButton);
 
         Container pane = this.getContentPane(); //add to JPane toolbar
