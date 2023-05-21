0a8d3d99e8b477af139a829af8ef1284da1598be
Daniel <daniel_tzonkov@abv.bg>
2023-05-21 12:19:05 +0300

    Update

--- src/MultidimensionalArrays/FillTheMatrix.java
+++ src/MultidimensionalArrays/FillTheMatrix.java
@@ -6,6 +6,56 @@ public class FillTheMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split(", ");
+        int size = Integer.parseInt(input[0]);
+        String type = input[1];
 
+        int[][] matrix = new int[size][size];
+
+        if (type.equals("A")) {
+            fillPatternA(matrix);
+        } else {
+            fillPatternB(matrix);
+        }
+
+        printMatrix(matrix);
+
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillPatternA(int[][] matrix) {
+        int fillValue = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[j][i] = fillValue;
+                fillValue++;
+            }
+        }
+    }
+
+    private static void fillPatternB(int[][] matrix) {
+        int fillValue = 1;
+
+        for (int i = 0; i < matrix.length; i++) {
+            if(i % 2 == 0) {
+                for (int j = 0; j < matrix.length; j++) {
+                    matrix[j][i] = fillValue;
+                    fillValue++;
+                }
+            }else {
+                for (int j = matrix.length - 1; j >= 0; j--) {
+                    matrix[j][i] = fillValue;
+                    fillValue++;
+                }
+            }
+        }
     }
 }
