a7dfb815ca7b541fd90cd844fe9974aa076dc084
Daniel <daniel_tzonkov@abv.bg>
2023-05-22 20:30:34 +0300

    Update

---
+++ src/MultidimensionalArrays/StringMatrixRotation.java
@@ -0,0 +1,11 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class StringMatrixRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
