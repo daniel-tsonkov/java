f97d61dcf60c66f3dc5859654a28562ed3c95235
Daniel <daniel_tzonkov@abv.bg>
2023-05-22 10:11:02 +0300

    Update

---
+++ src/MultidimensionalArrays/DiagonalDifference.java
@@ -0,0 +1,11 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class DiagonalDifference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
