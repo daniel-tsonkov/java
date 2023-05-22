ccca25f8f0a7588335fed5ce22318c637c9d6eef
Daniel <daniel_tzonkov@abv.bg>
2023-05-22 20:12:58 +0300

    Update

--- src/MultidimensionalArrays/MaximalSum.java
+++ src/MultidimensionalArrays/MaximalSum.java
@@ -1,11 +1,36 @@
 package MultidimensionalArrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaximalSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tokens = scanner.nextLine().split("\\s+");
+        int rows = Integer.parseInt(tokens[0]);
+        int cols = Integer.parseInt(tokens[1]);
 
+        int[][] matrix = new int[rows][cols];
+
+        fillMatrix(matrix, scanner);
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int row = 0; row < rows - 2; row++) {
+            for (int col = 0; col < cols - 2; col++) {
+                int sum = matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2] + matrix[row + 1][col] + matrix[row + 1][col + 1] + matrix[row + 1][col + 2] + matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+    }
+
+    private static void fillMatrix(int[][] matrix, Scanner scanner) {
+        for (int row = 0; row < matrix.length; row++) {
+            matrix[row] = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer :: parseInt).toArray();
+        }
     }
 }
