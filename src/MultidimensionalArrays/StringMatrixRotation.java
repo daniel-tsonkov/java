5f9e6543f9275baf1bc4ff56a9646465f5cd6411
Daniel <daniel_tzonkov@abv.bg>
2023-05-22 21:00:26 +0300

    Update

--- src/MultidimensionalArrays/StringMatrixRotation.java
+++ src/MultidimensionalArrays/StringMatrixRotation.java
@@ -1,11 +1,28 @@
 package MultidimensionalArrays;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StringMatrixRotation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int rotation = Integer.parseInt(scanner.nextLine().split("[()}]")[1]);
 
+        String input = scanner.nextLine();
+        List<String> words = new ArrayList<>();
+        int maxLength = Integer.MIN_VALUE;
+
+        while (!input.equals("END")) {
+            if (input.length() > maxLength) {
+                maxLength = input.length();
+            }
+            input = scanner.nextLine();
+        }
+        int rows = words.size();
+        int cols = maxLength;
+
+        char[][] matrix = new char[rows][cols];
     }
 }
