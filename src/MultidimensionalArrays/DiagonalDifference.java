2872c0e5396770944621a91b14bf53ecdbbd505d
Daniel <daniel_tzonkov@abv.bg>
2023-05-22 10:24:17 +0300

    Update

--- src/MultidimensionalArrays/DiagonalDifference.java
+++ src/MultidimensionalArrays/DiagonalDifference.java
@@ -6,6 +6,21 @@ public class DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sizeOfMatrix = Integer.parseInt(scanner.nextLine());
+        int[][] myMatrix = new int[sizeOfMatrix][sizeOfMatrix];
+
+
+        for (int i = 0; i < sizeOfMatrix; i++) {
+            String[] rowValues = scanner.nextLine().split("\\s+");
+            for (int j = 0; j < sizeOfMatrix; j++) {
+                myMatrix[i][j] = Integer.parseInt(rowValues[j]);
+            }
+        }
+
+        getDiagonalValue(myMatrix);
+    }
+
+    private static void getDiagonalValue(int[][] myMatrix) {
 
     }
 }
