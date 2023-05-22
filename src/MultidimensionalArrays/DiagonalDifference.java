358541410c5d2c14ba6e895eb2c6445aaf998b62
Daniel <daniel_tzonkov@abv.bg>
2023-05-22 10:44:48 +0300

    Update

--- src/MultidimensionalArrays/DiagonalDifference.java
+++ src/MultidimensionalArrays/DiagonalDifference.java
@@ -9,7 +9,6 @@ public class DiagonalDifference {
         int sizeOfMatrix = Integer.parseInt(scanner.nextLine());
         int[][] myMatrix = new int[sizeOfMatrix][sizeOfMatrix];
 
-
         for (int i = 0; i < sizeOfMatrix; i++) {
             String[] rowValues = scanner.nextLine().split("\\s+");
             for (int j = 0; j < sizeOfMatrix; j++) {
@@ -17,10 +16,27 @@ public class DiagonalDifference {
             }
         }
 
-        getDiagonalValue(myMatrix);
+        int primariDiagonal = getPrimaryDiagonalValue(myMatrix);
+        int secondaryDiagonal = getSecondaryDiagonalValue(myMatrix);
+                ;
+
+        System.out.println(Math.abs(primariDiagonal - secondaryDiagonal));
+
     }
 
-    private static void getDiagonalValue(int[][] myMatrix) {
+    private static int getPrimaryDiagonalValue(int[][] myMatrix) {
+        int primariDiagonal = 0;
+        for (int i = 0; i < myMatrix.length; i++) {
+            primariDiagonal +=  myMatrix[i][i];
+        }
+        return primariDiagonal;
+    }
 
+    private static int getSecondaryDiagonalValue(int[][] myMatrix) {
+        int secondaryDiagonal = 0;
+        for (int i = 0; i < myMatrix.length; i++) {
+            secondaryDiagonal +=  myMatrix[myMatrix.length - i - 1][i];
+        }
+        return secondaryDiagonal;
     }
 }
