e2e175ef9eb82f2743be87219bb3c0b4fa74ac9a
Daniel <daniel_tzonkov@abv.bg>
2023-05-23 12:21:05 +0300

    Update

--- src/MultidimensionalArrays/StringMatrixRotation.java
+++ src/MultidimensionalArrays/StringMatrixRotation.java
@@ -9,12 +9,14 @@ public class StringMatrixRotation {
         Scanner scanner = new Scanner(System.in);
 
         int rotation = Integer.parseInt(scanner.nextLine().split("[()}]")[1]);
+        int rotations = rotation % 360;
 
         String input = scanner.nextLine();
         List<String> words = new ArrayList<>();
         int maxLength = Integer.MIN_VALUE;
 
         while (!input.equals("END")) {
+            words.add(input);
             if (input.length() > maxLength) {
                 maxLength = input.length();
             }
@@ -24,5 +26,59 @@ public class StringMatrixRotation {
         int cols = maxLength;
 
         char[][] matrix = new char[rows][cols];
+
+        fill(matrix, words);
+
+        switch (rotations) {
+            case 90:
+                matrix = rotate90(matrix, rows, cols);
+                break;
+            case 180:
+                matrix = rotate90(matrix, rows, cols);
+                matrix = rotate90(matrix, cols, rows);
+                break;
+            case 270:
+                matrix = rotate90(matrix, rows, cols);
+                matrix = rotate90(matrix, cols, rows);
+                matrix = rotate90(matrix, rows, cols);
+                break;
+        }
+        printMatrix(matrix);
+
+    }
+
+    public static void fill(char[][] matrix, List<String> words) {
+        for (int row = 0; row < matrix.length; row++) {
+            String currentWord = words.get(row);
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (col < currentWord.length()) {
+                    matrix[row][col] = currentWord.charAt(col);
+                } else {
+                    matrix[row][col] = ' ';
+                }
+            }
+        }
+    }
+
+    private static char[][] rotate90(char[][] matrix, int rows, int cols) {
+        char[][] rotatedMatrix = new char[cols][rows];
+
+        for (int col = 0; col < cols; col++) {
+            int c = 0;
+            for (int row = rows - 1; row >= 0; row--) {
+                rotatedMatrix[col][c++] = matrix[row][col];
+            }
+        }
+
+        return rotatedMatrix;
+    }
+
+    private static void printMatrix(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
