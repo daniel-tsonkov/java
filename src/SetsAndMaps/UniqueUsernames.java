03e584c5e8bbbeb1878c1e3432be1ef700e07ca2
Daniel <daniel_tzonkov@abv.bg>
2023-05-24 21:04:34 +0300

    Update

--- src/SetsAndMaps/UniqueUsernames.java
+++ src/SetsAndMaps/UniqueUsernames.java
@@ -1,11 +1,23 @@
 package SetsAndMaps;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class UniqueUsernames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> usernames = new LinkedHashSet<>();
 
+        for (int i = 0; i < n; i++) {
+            String username = scanner.nextLine();
+            usernames.add(username);
+        }
+
+        for (String username : usernames) {
+            System.out.println(username);
+        }
     }
 }
