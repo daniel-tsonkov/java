456265429d07d933e35f0b14975015e8e1ac67d0
Daniel <daniel_tzonkov@abv.bg>
2023-05-25 20:05:29 +0300

    Update

--- src/SetsAndMaps/HandsOfCards.java
+++ src/SetsAndMaps/HandsOfCards.java
@@ -3,15 +3,29 @@ package SetsAndMaps;
 import java.util.*;
 
 public class HandsOfCards {
-    private  static Map<String, Integer> multiplaiers = new HashMap<>();
-    //private static LinkedList<String> cardPowers = (LinkedList<String>) Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A");
-    private static Map<String, String> cardsPowers = new HashMap<>();
+    private static Map<String, Integer> multiplaiers = new HashMap<>();
+    private static Map<String, Integer> cardsPowers = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         multiplaiers.put("S", 4);
         multiplaiers.put("H", 3);
         multiplaiers.put("D", 2);
         multiplaiers.put("C", 1);
+
+        cardsPowers.put("2", 2);
+        cardsPowers.put("3", 3);
+        cardsPowers.put("4", 4);
+        cardsPowers.put("5", 5);
+        cardsPowers.put("6", 6);
+        cardsPowers.put("7", 7);
+        cardsPowers.put("8", 8);
+        cardsPowers.put("9", 9);
+        cardsPowers.put("10", 10);
+        cardsPowers.put("J", 11);
+        cardsPowers.put("Q", 12);
+        cardsPowers.put("K", 13);
+        cardsPowers.put("A", 14);
 
         String input = scanner.nextLine();
         Map<String, HashSet<String>> players = new LinkedHashMap<>();
@@ -20,10 +34,11 @@ public class HandsOfCards {
             String[] tokens = input.split(": ");
             String name = tokens[0];
 
-            if(!players.containsKey(name)) {
+            if (!players.containsKey(name)) {
                 players.put(name, new HashSet<>());
             }
             String[] cards = tokens[1].split(", ");
+
             for (String card : cards) {
                 players.get(name).add(card);
             }
@@ -32,16 +47,19 @@ public class HandsOfCards {
         }
 
         players.forEach((k, v) -> {
-          String player = k;
-          calculatePoint(v);
+            int points = calculatePoint(v);
+            System.out.printf("%s: %d\n", k, points);
         });
+
     }
 
-    private static void calculatePoint(HashSet<String> cards) {
+    private static int calculatePoint(HashSet<String> cards) {
         int point = 0;
-        for(String card : cards) {
-            String power =  card.substring(card.length() - 1);
+        for (String card : cards) {
+            String power = card.substring(0, card.length() - 1);
             String multiplayer = card.substring(card.length() - 1);
+            point += cardsPowers.get(power) * multiplaiers.get(multiplayer);
         }
+        return point;
     }
 }
