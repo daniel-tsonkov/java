275380dda83e770e96bfac3d34d66ff75a18e7c6
Daniel <daniel_tzonkov@abv.bg>
2023-05-25 16:17:41 +0300

    Update

--- src/SetsAndMaps/FixEmails.java
+++ src/SetsAndMaps/FixEmails.java
@@ -1,11 +1,27 @@
 package SetsAndMaps;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class FixEmails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, String> records = new LinkedHashMap<>();
+        String input = scanner.nextLine();
 
+        while (!input.equals("stop")) {
+            String email = scanner.nextLine();
+            String getExtension =  email.substring(email.lastIndexOf('.') + 1).toLowerCase();
+
+            if(!getExtension.equals("us") && !getExtension.equals("uk") && !getExtension.equals("com")){
+                records.put(input, email);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        records.forEach((k, v) -> System.out.printf("%s -> %s\n", k, v));
     }
 }
