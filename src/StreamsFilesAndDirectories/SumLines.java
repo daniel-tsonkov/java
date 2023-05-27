2eb0b420280e8d6be97bbb5cd2e5fbb99f537a7f
Daniel <daniel_tzonkov@abv.bg>
2023-05-27 11:14:08 +0300

    Update

--- src/StreamsFilesAndDirectories/SumLines.java
+++ src/StreamsFilesAndDirectories/SumLines.java
@@ -1,7 +1,33 @@
 package StreamsFilesAndDirectories;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class SumLines {
     public static void main(String[] args) {
+        String path = "C:\\Users\\Daniel\\IdeaProjects\\java\\src\\StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                char[] chars = line.toCharArray();
+                long sum = 0;
+
+                for (char aChar : chars) {
+                    sum += aChar;
+                }
+                System.out.println(sum);
+
+                line = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
