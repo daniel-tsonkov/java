4644912dfed9864f84a27758b6830ef52c864a2b
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-05-30 14:58:54 +0300

    bb

--- src/FunctionalProgramming/ConsumerPrint.java
+++ src/FunctionalProgramming/ConsumerPrint.java
@@ -1,7 +1,20 @@
 package FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class ConsumerPrint {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String[] strings = scanner.nextLine().split("\\s+");
+
+        /*for (String word : strings) {
+            print.accept(word);
+        }*/
+
+        Consumer<String> print = word -> System.out.println(word);
+        Arrays.stream(strings).forEach(print);
     }
 }
