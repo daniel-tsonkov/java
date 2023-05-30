275c5a3a0e30d492e8dd39f79c999f95cd82d364
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-05-30 15:11:01 +0300

    bb

--- src/FunctionalProgramming/KnightsOfHonor.java
+++ src/FunctionalProgramming/KnightsOfHonor.java
@@ -1,6 +1,8 @@
 package FunctionalProgramming;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 import static java.lang.System.in;
 
@@ -8,6 +10,10 @@ public class KnightsOfHonor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
 
+        String[] names = scanner.nextLine().split("\\s+");
 
+        Consumer<String> apendSir = name -> System.out.println("Sir " + name);
+
+        Arrays.stream(names).forEach(apendSir);
     }
 }
