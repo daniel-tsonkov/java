7ee6c268f0f115738b9bcf10e3f57bb70f9d54d4
Daniel <daniel_tzonkov@abv.bg>
2023-05-30 22:53:30 +0300

    Update

---
+++ src/FunctionalProgramming/ReverseAndExclude.java
@@ -0,0 +1,11 @@
+package FunctionalProgramming;
+
+import java.util.Scanner;
+
+public class ReverseAndExclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
