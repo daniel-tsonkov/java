831b4528496b55561140436a90484d70313daa1f
Daniel <daniel_tzonkov@abv.bg>
2023-05-30 22:07:33 +0300

    Update

---
+++ src/FunctionalProgramming/AppliedArithmetic.java
@@ -0,0 +1,11 @@
+package FunctionalProgramming;
+
+import java.util.Scanner;
+
+public class AppliedArithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
