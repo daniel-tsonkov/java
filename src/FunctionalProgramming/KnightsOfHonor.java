fa163db4f839d9a0dbd8f678ff1415beb8685b91
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-05-30 15:02:56 +0300

    bb

---
+++ src/FunctionalProgramming/KnightsOfHonor.java
@@ -0,0 +1,13 @@
+package FunctionalProgramming;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+
+
+    }
+}
