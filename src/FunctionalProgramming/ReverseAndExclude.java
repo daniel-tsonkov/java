7ebcf95a088651d2400719ec784f2f676bb2e293
Daniel <daniel_tzonkov@abv.bg>
2023-06-01 21:20:59 +0300

    Update

--- src/FunctionalProgramming/ReverseAndExclude.java
+++ src/FunctionalProgramming/ReverseAndExclude.java
@@ -1,11 +1,28 @@
 package FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ReverseAndExclude {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(numbers);
+        Predicate<Integer> checkIsDivisible = num -> num % n == 0;
+        List<Integer> toRemove = numbers.stream().filter(e -> checkIsDivisible.test(e)).collect(Collectors.toList());
+
+        for(Integer num : toRemove) {
+            numbers.remove(num);
+        }
+
+        numbers.forEach((e -> System.out.print(e + " ")));
     }
 }
