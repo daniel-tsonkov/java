7979fd17e3ce8bb9a4cf2bded12155abe0c0a293
Daniel <daniel_tzonkov@abv.bg>
2023-06-01 21:24:16 +0300

    Update

---
+++ src/FunctionalProgramming/FindTheSmallestElement.java
@@ -0,0 +1,11 @@
+package FunctionalProgramming;
+
+import java.util.Scanner;
+
+public class FindTheSmallestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
