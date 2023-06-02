e3642b9a8641871ae2776473b4312b3b1186e914
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-06-02 15:03:00 +0300

    bb

--- src/FunctionalProgramming/FindTheSmallestElement.java
+++ src/FunctionalProgramming/FindTheSmallestElement.java
@@ -1,11 +1,21 @@
 package FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FindTheSmallestElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Function<List<Integer>, Integer> findSmallestNumber = list -> Collections.min(list);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
 
+        Integer smallestNumber = findSmallestNumber.apply(numbers);
+
+        System.out.println(numbers.lastIndexOf(smallestNumber));
     }
 }
