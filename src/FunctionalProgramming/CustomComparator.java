47e8573e7c5c8398868fbaa9b6f368a9cdc72bc3
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-06-02 15:09:54 +0300

    bb

---
+++ src/FunctionalProgramming/CustomComparator.java
@@ -0,0 +1,13 @@
+package FunctionalProgramming;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+
+
+    }
+}
